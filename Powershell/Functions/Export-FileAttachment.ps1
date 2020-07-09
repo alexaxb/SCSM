cea5ee9eaf9ee4a09b08fd06acffd33dd88b3511
alexaxb <alex_@PC1B49EE.headquarters.innofactor.com>
2020-07-09 17:27:49 +0200

    Support for multiple files

--- Powershell/Functions/Export-FileAttachment.ps1
+++ Powershell/Functions/Export-FileAttachment.ps1
@@ -5,6 +5,7 @@ function Export-FileAttachment {
     )
 
     Try {
+    $WorkItemId = $WorkItemObject.Id
     #Create folder for IR
     New-Item -ItemType directory -Path $($OutputFolder + $WorkItemId) -Force | Out-Null
     
@@ -13,29 +14,31 @@ function Export-FileAttachment {
         Return
     }
 
-    #File byte buffer
-    $buffer = new-object byte[] -ArgumentList 4096
+    foreach ($obj in $FileObject){
+        #File byte buffer
+        $buffer = new-object byte[] -ArgumentList 4096
 
-    $DisplayName = ($FileObject.Values | Where-Object {$_.type -match "DisplayName"}).value 
-    $Content =  ($FileObject.Values | Where-Object {$_.type -match "Content"}).value # as Microsoft.EnterpriseManagement.Common.ServerBinaryStream
+        $DisplayName = ($obj.Values | Where-Object {$_.type -match "DisplayName"}).value 
+        $Content =  ($obj.Values | Where-Object {$_.type -match "Content"}).value # as Microsoft.EnterpriseManagement.Common.ServerBinaryStream
 
-	#i.e. "2012.08.24 12.18.PM CR7050.xml"
-    $filepath = ($OutputFolder + "$WorkItemId\" + $DisplayName)
+        #i.e. "2012.08.24 12.18.PM CR7050.xml"
+        $filepath = ($OutputFolder + "$WorkItemId\" + $DisplayName)
 
-    $stream = new-object System.IO.FileStream($filePath,[System.IO.FileMode]'Create',[System.IO.FileAccess]'Write')
+        $stream = new-object System.IO.FileStream($filePath,[System.IO.FileMode]'Create',[System.IO.FileAccess]'Write')
 
-	#Loop through the server content stream, copying bytes into the buffer, 4k at a time, until there are no more bytes
-    $ReadBytes = 0
-    do {
-        if ($ReadBytes -Ne 0) {
-            $Stream.Write($buffer,0,$ReadBytes)
-        }
-        $ReadBytes = $Content.Read($Buffer, 0,4096)
-    } until ($ReadBytes -eq 0)
+        #Loop through the server content stream, copying bytes into the buffer, 4k at a time, until there are no more bytes
+        $ReadBytes = 0
+        do {
+            if ($ReadBytes -Ne 0) {
+                $Stream.Write($buffer,0,$ReadBytes)
+            }
+            $ReadBytes = $Content.Read($Buffer, 0,4096)
+        } until ($ReadBytes -eq 0)
 
-    #clean up
-    $Stream.Close()
-    $content.Close()
+        #clean up
+        $Stream.Close()
+        $content.Close()
+    }
     }
     Catch {
         Throw "Export-Attachment: Error exporting attachment $($_)"
